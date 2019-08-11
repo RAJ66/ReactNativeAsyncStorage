2f020689189b8fcdf01f3b1633d71d05968ea263
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-08-11 15:17:34 +0100

    Input complete

--- App.js
+++ App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import { AsyncStorage} from '@react-native-community/async-storage'
+import React, { Component } from 'react';
+import { AsyncStorage } from '@react-native-community/async-storage'
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
+  TextInput,
   StatusBar,
 } from 'react-native';
 
 
 
-const App = () => {
-  return (
-    <Text>Hello world</Text>
-  );
-};
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      textInputData: '',
+      //to get the value from the TextInput
+      getValue: '',
+      //to set the value on Text
+    };
+  }
+  render() {
+    return (
+      <View>
+        <Text>Hello world</Text>
+        <TextInput
+          style={styles.TextInputStyle}
+
+        ></TextInput>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  
+  TextInputStyle: {
+    textAlign: 'center',
+    height: 40,
+    width: '100%',
+    borderWidth: 1,
+    backgroundColor: 'orange',
+  },
 });
 
-export default App;
+
