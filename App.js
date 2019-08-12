905f910d8c7baf06278f67c62390e1eae1a9d827
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-08-12 15:05:46 +0100

    saved complete

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import { AsyncStorage } from '@react-native-community/async-storage'
+import  AsyncStorage  from '@react-native-community/async-storage'
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
-  Text,
   TextInput,
-  StatusBar,
+  Text,
+  TouchableOpacity,
 } from 'react-native';
 
 
@@ -22,14 +20,41 @@ export default class App extends Component {
       //to set the value on Text
     };
   }
+
+  //funcao para guardar o valor 
+  saveValueFunction = () => {
+    //ver se nao e null
+    if (this.state.textInputData) {
+      AsyncStorage.setItem('any_key_here', this.state.textInputData);
+      //meter outra vez o textInputData sem nada
+      this.setState({ textInputData: '' });
+      //alerta de consegiuu guardar
+      alert('Saved');
+    } else {
+      alert('Please fill data');
+
+    }
+  };
+
+
   render() {
     return (
       <View>
         <Text>Hello world</Text>
         <TextInput
+          value={this.state.textInputData}
           style={styles.TextInputStyle}
-
+          //troca o valor de textInputData
+          onChangeText={data => this.setState({ textInputData: data })}
+          placeholder="Enter Some Text here"
         ></TextInput>
+
+
+        <TouchableOpacity
+          onPress={this.saveValueFunction}
+          style={styles.button}>
+          <Text style={styles.buttonText}> SAVE VALUE </Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -42,6 +67,17 @@ const styles = StyleSheet.create({
     width: '100%',
     borderWidth: 1,
     backgroundColor: 'orange',
+  },
+  button: {
+    width: '100%',
+    height: 40,
+    padding: 10,
+    backgroundColor: '#848484',
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
   },
 });
 
